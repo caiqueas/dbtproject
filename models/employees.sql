78e55226c9702dd89934890e1abec72c786502b4
Caique Silva <caique.araujo@yahoo.com>
2026-04-06 16:31:35 +0000

    Versão 1.0

---
+++ models/employees.sql
@@ -0,0 +1,10 @@
+with calc_employees as(
+    select
+    date_part(year, current_date) - date_part(year, birth_date) as age,
+    extract(year from current_date) - extract(year from hire_date) as length_of_service,
+    concat(first_name ,' ', last_name) as full_name, *
+    from {{ source('sources', 'employees') }}
+
+)
+
+select * from calc_employees
